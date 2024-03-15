2189c8edc749480efaf84d7d7ff662cbb2ce4400
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2024-03-15 12:50:30 -0400

    futz around with sounds, and start the project

--- DevonRogersAlgorithmAssignment2/DevonRogersAlgorithmAssignment2.cpp
+++ DevonRogersAlgorithmAssignment2/DevonRogersAlgorithmAssignment2.cpp
@@ -2,10 +2,54 @@
 //
 
 #include <iostream>
+#pragma comment(lib, "winmm.lib")//required to make the audio play
+#include <Windows.h>//plays the audio, doesn't work in online compiler (hunt down functions in the header files)
+#include <vector>
+
+
+//audio is mostly figured out; it plays, but doesn't yet work in online compiler
+
+using namespace std;
+
+//prints all of the data in the vector that is passed in and wraps it in square brackets
+void printArray(vector<int> data) {
+    if (data.empty()) {
+        cout << "[]";
+        return;
+    }
+    cout << "[ ";
+    cout << data[0];
+    for (int i = 1; i < data.size();i++) {
+        cout << ", ";
+        cout << data[i];
+    }
+    cout << " ]";
+    return;
+}
+
+//the function that merges the arrays together
+void merge() {
+
+}
+
+//the mergesort algorithm itself
+void mergeSort(vector<int> arr, int const first, int const last) {//either make it void or vector<int>, not sure which yet
+    
+    vector<int> arr1, arr2;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Please enter the array you would like to sort. Please separate the values with commas.\n";
+    
+    
+    
+    
+    
+    
+    //this is stored here so that i remember it later
+    PlaySound(TEXT("SystemHand"), NULL, SND_ALIAS);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
